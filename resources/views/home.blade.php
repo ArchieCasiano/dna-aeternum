a3ca8431a410d5c95d16bb03deb71e8aa3de4817
Israel Breta <i_israel.breta@stratpoint.com>
2025-06-28 13:30:22 +0800

    [INITIAL] Fronend Commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Test Home</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body style="background: whitesmoke">
+    <h1>Starting point of everything!</h1>
+</body>
+
+</html>

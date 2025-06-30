d935b747b234fd05b2aaba5b20b2e146cca2e1cc
Israel Breta <i_israel.breta@stratpoint.com>
2025-07-01 02:55:39 +0800

    [ADDED] Kits page with placeholders

---
+++ resources/views/dashboard/kits.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1 class="text-2xl font-bold mb-4">Kits</h1>
+    <p class="text-gray-600">Placeholder for adding, viewing, and assigning users to kits.</p>
+@endsection
